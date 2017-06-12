1125b96d26ebbb835d0df4667dbd2e48fb0440df
Jan Redmann <janredmann1@gmail.com>
2017-06-12 02:11:23 +0200

    Add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', "TodoController@index");
+Route::post('/', "TodoController@create");
+Route::delete('/', 'TodoController@deleteAll');
+Route::get('/{id}', "TodoController@get");
+Route::patch('/{id}', "TodoController@update");
+Route::delete('/{id}', "TodoController@delete");
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
