45c202cb59ca40cbfc0719577f8896bb76216b88
Jan Redmann <janredmann1@gmail.com>
2017-06-12 02:38:00 +0200

    Cast completed attribute to boolean in Todo model

--- app/Todo.php
+++ app/Todo.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $guarded = [];
+
+    protected $attributes = [
+        'completed' => false,
+    ];
+
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
 }
